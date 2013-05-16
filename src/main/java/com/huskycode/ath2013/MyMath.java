4b510b980710617b3fba1e7fba177121cb687b6c
Varokas Panusuwan <varokas@panusuwan.com>
2013-05-16 04:26:57 -0700

    add a class and update build.gradle

---
+++ src/main/java/com/huskycode/ath2013/MyMath.java
@@ -0,0 +1,11 @@
+package com.huskycode.ath2013;
+
+public class MyMath {
+    public static int sum(int n) {
+        return 0;
+    }
+
+    public static int fib(int n) {
+        return 0;
+    }
+}
